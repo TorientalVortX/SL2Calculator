4fdab136c076feebfbca887ec7e7a789c44ff018
Christopher L. <83620137+TorientalVortX@users.noreply.github.com>
2025-10-01 20:20:12 -0500

    Add files via upload

---
+++ Properties/Resources.cs
@@ -0,0 +1,508 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SL2_Reckoning_Calculator.Properties.Resources
+// Assembly: SL2 Reckoning Calculator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: DA3BB676-BB0B-4853-B9BA-79F2857E373B
+// Assembly location: C:\Users\chris\Downloads\SL2 Reckoning Calculator updated.exe
+
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+namespace SL2_Reckoning_Calculator.Properties;
+
+[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+[DebuggerNonUserCode]
+[CompilerGenerated]
+internal class Resources
+{
+  private static ResourceManager resourceMan;
+  private static CultureInfo resourceCulture;
+
+  internal Resources()
+  {
+  }
+
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  internal static ResourceManager ResourceManager
+  {
+    get
+    {
+      if (SL2_Reckoning_Calculator.Properties.Resources.resourceMan == null)
+        SL2_Reckoning_Calculator.Properties.Resources.resourceMan = new ResourceManager("SL2_Reckoning_Calculator.Properties.Resources", typeof (SL2_Reckoning_Calculator.Properties.Resources).Assembly);
+      return SL2_Reckoning_Calculator.Properties.Resources.resourceMan;
+    }
+  }
+
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  internal static CultureInfo Culture
+  {
+    get => SL2_Reckoning_Calculator.Properties.Resources.resourceCulture;
+    set => SL2_Reckoning_Calculator.Properties.Resources.resourceCulture = value;
+  }
+
+  internal static Bitmap AkurZoOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AkurZoOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap AkurZoOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AkurZoOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap AllOff
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AllOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap AllOn
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AllOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap AxysAlOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AxysAlOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap AxysAlOff1
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AxysAlOff1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap AxysAlOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AxysAlOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap AxysAlOn1
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (AxysAlOn1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap BldiIaOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (BldiIaOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap BldiIaOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (BldiIaOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap cel1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (cel1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap cel1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (cel1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap ChoiErOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (ChoiErOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap ChoiErOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (ChoiErOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap dark1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (dark1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap dark1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (dark1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap earth1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (earth1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap earth1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (earth1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap fai1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (fai1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap fai1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (fai1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap fai2
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (fai2), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap fai2green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (fai2green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap GrenUtOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (GrenUtOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap GrenUtOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (GrenUtOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap HolyMrOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (HolyMrOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap HolyMrOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (HolyMrOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap I_NEED_CALC2
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject("I NEED CALC2", SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap Jupiter
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Jupiter), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap KagiJiOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (KagiJiOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap KagiJiOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (KagiJiOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap KashIcOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (KashIcOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap KashIcOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (KashIcOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap light1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (light1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap light1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (light1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap LunaCuOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (LunaCuOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap LunaCuOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (LunaCuOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap Mars
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Mars), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap Mercury
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Mercury), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap minus
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (minus), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap Neptune
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Neptune), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap NoSign
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (NoSign), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap plus
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (plus), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap Pluto
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Pluto), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap RabeUrOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (RabeUrOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap RabeUrOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (RabeUrOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap Saturn
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Saturn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap ski1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (ski1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap ski1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (ski1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap stamps2
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (stamps2), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap Starsign
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Starsign), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap str1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (str1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap str1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (str1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap tokiko
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (tokiko), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap Uranus
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Uranus), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap Venus
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (Venus), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap vit1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (vit1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap vit1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (vit1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap vit2
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (vit2), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap vit2green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (vit2green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap vit3
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (vit3), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap vit3green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (vit3green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap water1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (water1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap water1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (water1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap wil1
+  {
+    get => (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (wil1), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+  }
+
+  internal static Bitmap wil1green
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (wil1green), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap ZeroGyOff
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (ZeroGyOff), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+
+  internal static Bitmap ZeroGyOn
+  {
+    get
+    {
+      return (Bitmap) SL2_Reckoning_Calculator.Properties.Resources.ResourceManager.GetObject(nameof (ZeroGyOn), SL2_Reckoning_Calculator.Properties.Resources.resourceCulture);
+    }
+  }
+}
---
+++ Properties/Settings.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: SL2_Reckoning_Calculator.Properties.Settings
+// Assembly: SL2 Reckoning Calculator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: DA3BB676-BB0B-4853-B9BA-79F2857E373B
+// Assembly location: C:\Users\chris\Downloads\SL2 Reckoning Calculator updated.exe
+
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+namespace SL2_Reckoning_Calculator.Properties;
+
+[CompilerGenerated]
+[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+internal sealed class Settings : ApplicationSettingsBase
+{
+  private static Settings defaultInstance = (Settings) SettingsBase.Synchronized((SettingsBase) new Settings());
+
+  public static Settings Default => Settings.defaultInstance;
+}
